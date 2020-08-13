2f1e6cfe7533f5c0594d119d69ef13f920b1f3dc
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-13 12:22:45 +0800

    backend init

---
+++ parser.js
@@ -0,0 +1,24 @@
+const nightmare = require("nightmare")();
+
+const args = process.argv.slice(2);
+const url = args[0];
+const minPrice = args[1];
+//priceblock_ourprice
+//https://www.amazon.com/BestMassage-Office-Desk-Gaming-Chair/dp/B01ICHJIM8
+
+checkPrice();
+
+async function checkPrice() {
+  const priceString = await nightmare
+    .goto(url)
+    .wait("#priceblock_ourprice")
+    .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+    .end();
+
+  const priceNumber = parseFloat(priceString.replace("$", ""));
+  if (priceNumber < minPrice) {
+    console.log("cheap");
+  } else {
+    console.log("expensive");
+  }
+}
