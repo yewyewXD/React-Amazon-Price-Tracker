8a9c1dad41f1fd6af273166e1aba690980d978bf
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-16 11:46:17 +0800

    validate and respond

--- frontend/src/TrackBar.js
+++ frontend/src/TrackBar.js
@@ -5,8 +5,17 @@ export default function TrackBar() {
   const urlElRef = useRef();
   const emailElRef = useRef();
 
-  function handlePostURL() {
-    if (urlElRef.current.value && emailElRef.current.value) {
+  function handlePostURL(e) {
+    e.preventDefault();
+
+    // Validation
+    if (
+      urlElRef.current.value &&
+      emailElRef.current.value &&
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        emailElRef.current.value
+      )
+    ) {
       const email = emailElRef.current.value;
       const url = urlElRef.current.value;
 
@@ -14,15 +23,18 @@ export default function TrackBar() {
       const trimmedUrl = url.substr(0, url.indexOf("/ref="));
 
       // Fetch info to backend
-      axios.post("http://localhost:5000/track", {
-        url:
-          trimmedUrl.indexOf("https://") === -1
-            ? "https://" + trimmedUrl
-            : trimmedUrl,
-        minPrice: 10,
-        redirect: true,
-        email,
-      });
+      axios
+        .post("http://localhost:5000/track", {
+          url:
+            trimmedUrl.indexOf("https://") === -1
+              ? "https://" + trimmedUrl
+              : trimmedUrl,
+          minPrice: 10,
+          email,
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
 
       urlElRef.current.value = "";
       emailElRef.current.value = "";
@@ -33,33 +45,35 @@ export default function TrackBar() {
 
   return (
     <div>
-      <div className="form-row">
-        <div className="col-4">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Paste your url here"
-            ref={urlElRef}
-          />
+      <form>
+        <div className="form-row">
+          <div className="col-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Paste your url here"
+              ref={urlElRef}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="form-row">
-        <div className="col-4">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter your email here"
-            ref={emailElRef}
-          />
+        <div className="form-row">
+          <div className="col-4">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter your email here"
+              ref={emailElRef}
+            />
+          </div>
         </div>
-      </div>
-      <button
-        className="btn btn-secondary btn-large py-1"
-        onClick={handlePostURL}
-      >
-        Track
-      </button>
+        <button
+          className="btn btn-secondary btn-large py-1"
+          onClick={handlePostURL}
+        >
+          Track
+        </button>
+      </form>
     </div>
   );
 }
--- routes/track.js
+++ routes/track.js
@@ -3,32 +3,40 @@ const router = express.Router();
 const nightmare = require("nightmare")();
 
 router.route("/").post(async (req, res) => {
-  const { url, email, minPrice, redirect } = req.body;
-  console.log(req.body);
+  const { url, email, minPrice } = req.body;
+  res.json(req.body);
 
-  // const priceString = await nightmare
-  //   .goto(url)
-  //   .wait("#priceblock_ourprice")
-  //   .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
-  //   .end();
+  const priceString = await nightmare
+    .goto(url)
+    .wait("#priceblock_ourprice")
+    .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+    .end();
 
-  // const priceNumber = parseFloat(priceString.replace("$", ""));
-  // if (priceNumber < minPrice) {
-  //   console.log("cheap");
-  // } else {
-  //   console.log("expensive");
-  // }
+  const priceNumber = parseFloat(priceString.replace("$", ""));
+  if (priceNumber < minPrice) {
+    res.json("cheap");
+  } else {
+    res.json("expensive");
+  }
 
   // const mcData = {
-  //   members:[
+  //   members: [
   //     {
   //       email_address: email,
-  //       status,
-  //     }
-  //   ]
-  // }
+  //       status: "pending", // use "subscribed" if no verification needed
+  //     },
+  //   ],
+  // };
+  // const mcDataPost = JSON.stringify(mcData);
 
-  res.json("track success");
+  // const options = {
+  //   url: "",
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: "auth...",
+  //   },
+  //   body: mcDataPost,
+  // };
 });
 
 module.exports = router;
