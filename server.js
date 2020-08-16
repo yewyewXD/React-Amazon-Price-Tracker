5eb3948d9bd651144e79b659c4d483f61da371d5
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-16 11:46:42 +0800

    heroku production setup

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 // routes
 const trackRouter = require("./routes/track");
 
@@ -17,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/track", trackRouter);
+
 // port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
