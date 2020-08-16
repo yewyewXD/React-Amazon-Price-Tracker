9e5e8324ff6460b035ac1c3327d9adcddad194fb
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-16 10:06:55 +0800

    clean up and receive url

--- frontend/src/TrackBar.js
+++ frontend/src/TrackBar.js
@@ -3,21 +3,29 @@ import axios from "axios";
 
 export default function TrackBar() {
   const urlElRef = useRef();
+  const emailElRef = useRef();
 
   function handlePostURL() {
-    if (urlElRef.current.value) {
+    if (urlElRef.current.value && emailElRef.current.value) {
+      const email = emailElRef.current.value;
+      const url = urlElRef.current.value;
+
       // Trim URL
-      const newString = urlElRef.current.value.slice(8).split("/").slice(0, 4);
-      newString.unshift("https:/");
-      const url = newString.join("/");
+      const trimmedUrl = url.substr(0, url.indexOf("/ref="));
 
       // Fetch info to backend
       axios.post("http://localhost:5000/track", {
-        url: url,
-        price: 10,
+        url:
+          trimmedUrl.indexOf("https://") === -1
+            ? "https://" + trimmedUrl
+            : trimmedUrl,
+        minPrice: 10,
+        redirect: true,
+        email,
       });
 
       urlElRef.current.value = "";
+      emailElRef.current.value = "";
     } else {
       console.log("please enter something");
     }
@@ -27,15 +35,31 @@ export default function TrackBar() {
     <div>
       <div className="form-row">
         <div className="col-4">
-          <input type="text" className="form-control" ref={urlElRef} />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Paste your url here"
+            ref={urlElRef}
+          />
         </div>
-        <button
-          className="btn btn-secondary btn-large py-1"
-          onClick={handlePostURL}
-        >
-          Track
-        </button>
       </div>
+
+      <div className="form-row">
+        <div className="col-4">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter your email here"
+            ref={emailElRef}
+          />
+        </div>
+      </div>
+      <button
+        className="btn btn-secondary btn-large py-1"
+        onClick={handlePostURL}
+      >
+        Track
+      </button>
     </div>
   );
 }
--- routes/track.js
+++ routes/track.js
@@ -3,24 +3,32 @@ const router = express.Router();
 const nightmare = require("nightmare")();
 
 router.route("/").post(async (req, res) => {
-  const url = req.body.url;
-  const minPrice = req.body.price;
+  const { url, email, minPrice, redirect } = req.body;
   console.log(req.body);
 
-  const priceString = await nightmare
-    .goto(url)
-    .wait("#priceblock_ourprice")
-    .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
-    .end();
+  // const priceString = await nightmare
+  //   .goto(url)
+  //   .wait("#priceblock_ourprice")
+  //   .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+  //   .end();
 
-  const priceNumber = parseFloat(priceString.replace("$", ""));
-  if (priceNumber < minPrice) {
-    console.log("cheap");
-  } else {
-    console.log("expensive");
-  }
+  // const priceNumber = parseFloat(priceString.replace("$", ""));
+  // if (priceNumber < minPrice) {
+  //   console.log("cheap");
+  // } else {
+  //   console.log("expensive");
+  // }
 
-  res.json("successfully get the url");
+  // const mcData = {
+  //   members:[
+  //     {
+  //       email_address: email,
+  //       status,
+  //     }
+  //   ]
+  // }
+
+  res.json("track success");
 });
 
 module.exports = router;
