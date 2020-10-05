4e0d7d4f114875444e4c568d5465459a53f229cc
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-05 09:54:45 +0800

    add icons to feature section

--- frontend/src/components/Home/FeatureSection.jsx
+++ frontend/src/components/Home/FeatureSection.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { featureItemBg } from "../../images/homeImages";
+import featureItem1 from "../../images/featureProfile.png";
+import featureItem2 from "../../images/featureTrack.png";
+import featureItem3 from "../../images/featureUpdate.png";
 
 export default function FeatureSection() {
   const featureCols = [
     {
-      image: featureItemBg,
+      image: featureItem1,
       title: "Create an account",
       subtitle:
         "To access to your personal dashboard, you have to first login or create an account.",
     },
     {
-      image: featureItemBg,
+      image: featureItem2,
       title: "Track a new product",
       subtitle:
         "Go to any product detail page on Amazon, copy & paste the link into the Product URL field, label the record with a personalized name, enter your desired price, and run the trace.",
     },
     {
-      image: featureItemBg,
+      image: featureItem3,
       title: "Keep it updated",
       subtitle:
         "You can re-track all the recorded products in just one click. Do it once in a while to get the latest price and information of the products.",
@@ -28,11 +31,19 @@ export default function FeatureSection() {
         <h1 className="title m-0 mb-lg-5 my-lg-0 my-4">It's Easy to Use</h1>
         <div className="row">
           {featureCols.map((featureCol, index) => (
-            <div
-              className="column col-lg-4 all-center-column mt-lg-0 my-sm-5 my-4 "
-              key={index}
-            >
-              <img src={featureCol.image} alt={featureCol.title} />
+            <div className="column col-lg-4 mt-lg-0 my-sm-5 my-4 " key={index}>
+              <div className="image all-center">
+                <img
+                  src={featureItemBg}
+                  alt="TrackBase step item background"
+                  className="position-absolute"
+                />
+                <img
+                  src={featureCol.image}
+                  alt={featureCol.title}
+                  style={{ zIndex: 2 }}
+                />
+              </div>
               <div className="title bold mb-3 mt-4 ">{featureCol.title}</div>
               <div className="subtitle">{featureCol.subtitle}</div>
             </div>
