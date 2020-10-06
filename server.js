c8d09799bac901a2c10fec682c9635587d6dbc19
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-06 09:16:03 +0800

    enable compression

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const compression = require("compression");
 
 // middleware
 dotenv.config();
 connectDB();
 const app = express();
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
 const auth = require("./middleware/auth");
 
 // routes
