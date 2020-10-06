23b5f41b522725830e744eb95194b12a34dbf74b
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-06 08:03:43 +0800

    fully responsive

--- frontend/src/components/AppFooter.jsx
+++ frontend/src/components/AppFooter.jsx
@@ -3,14 +3,15 @@ import { AiOutlineMail } from "react-icons/ai";
 
 export default function AppFooter() {
   return (
-    <footer className="footer all-center">
-      <div className="my-lg-3 my-sm-2 my-3 all-center">
-        <span className="bold">TrackerBase</span> &copy; All Rights Reserved
+    <footer className="footer all-center text-center">
+      <div className="my-lg-3 my-sm-2 my-3 all-center flex-md-row flex-column">
+        <span className="mb-md-0 mb-2">
+          <span className="bold">TrackerBase</span> &copy; All Rights Reserved
+        </span>
         <a
-          className="position-absolute all-center m-0 mr-4 text-decoration-none text-dark"
+          className="report-problem all-center m-0 mr-md-4 text-decoration-none text-dark"
           role="button"
           href="mailto:yew6933@gmail.com"
-          style={{ right: 0 }}
         >
           <small className="m-0">
             <AiOutlineMail className="mr-1" /> Report Problem
