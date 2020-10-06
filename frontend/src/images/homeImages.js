d296a41f02ebc5fa564232f14e03da4b07d3aa80
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-06 09:09:37 +0800

    use image cdn

--- frontend/src/images/homeImages.js
+++ frontend/src/images/homeImages.js
@@ -1,6 +1,8 @@
 module.exports = {
-  heroBg: "https://html.crumina.net/html-utouch/img/clouds1.png",
-  heroVector: "https://html.crumina.net/html-utouch/img/slides1.png",
-  featureItemBg: "https://html.crumina.net/html-utouch/img/clouds9.png",
-  discoverBg: "https://html.crumina.net/html-utouch/img/clouds5.png",
+  heroBg:
+    "https://res.cloudinary.com/dnkkw3nqk/image/upload/v1601946477/trackerbase/clouds1_s7zehg.png",
+  heroVector:
+    "https://res.cloudinary.com/dnkkw3nqk/image/upload/v1601946500/trackerbase/slides1_linufl.png",
+  featureItemBg:
+    "https://res.cloudinary.com/dnkkw3nqk/image/upload/v1601946517/trackerbase/clouds9_vprvms.png",
 };
