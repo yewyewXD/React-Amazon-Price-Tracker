e42e3eef5c27692ce410ea2aa0a774b364c3274d
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-12 20:29:26 +0800

    render loader when logging in

--- frontend/src/components/Popup/Modals/LoginModal.jsx
+++ frontend/src/components/Popup/Modals/LoginModal.jsx
@@ -1,12 +1,11 @@
 import React, { useContext, useRef, useState } from "react";
 import { GlobalContext } from "../../../context/GlobalState";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-// import Loader from "react-loader-spinner";
+import Loader from "react-loader-spinner";
 
 export default function LoginModal({ handleClose }) {
-  const { loginUser, token, errMsg } = useContext(GlobalContext);
+  const { loginUser, token, errMsg, userLoading } = useContext(GlobalContext);
   const [isShowingPw, setIsShowingPw] = useState(false);
-  // const [isSubmitting, setIsSubmitting] = useState(false);
 
   const emailElRef = useRef();
   const pwElRef = useRef();
@@ -14,22 +13,13 @@ export default function LoginModal({ handleClose }) {
   function handleLoginUser(e) {
     e.preventDefault();
 
-    // if (isShowingPw) {
-    //   setIsShowingPw(false);
-    // }
-
     const email = emailElRef.current.value ? emailElRef.current.value : "";
     const pw = pwElRef.current.value ? pwElRef.current.value : "";
 
     loginUser(email, pw);
 
-    // if (!token) {
-    //   setIsSubmitting(true);
-    // }
-
     if (token) {
       handleClose();
-      // setIsSubmitting(false);
     }
   }
 
@@ -43,78 +33,75 @@ export default function LoginModal({ handleClose }) {
 
     loginUser(email, pw);
 
-    // if (!token) {
-    //   setIsSubmitting(true);
-    // }
-
     if (token) {
       handleClose();
-      // setIsSubmitting(false);
     }
   }
 
   return (
-    // <>
-    //   {!token && isSubmitting && (
-    //     <div className="all-center-column">
-    //       <Loader type="Puff" color="#5fb0e5" height={200} width={200} />
-    //       <span className="mt-3 text-center">
-    //         Please wait while we're logging you in
-    //       </span>
-    //     </div>
-    //   )}
-
-    // {/* {!isSubmitting && ( */}
     <>
-      <h2 className="bold mb-4">Log In</h2>
-      <form className="form" onSubmit={handleLoginUser}>
-        <div className="form-group">
-          <label htmlFor="email" className="bold d-block">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            required
-            ref={emailElRef}
-          />
-        </div>
-
-        <div className="form-group mb-1">
-          <label htmlFor="password" className="bold d-block">
-            Password
-          </label>
-          <input
-            type={isShowingPw ? "text" : "password"}
-            className="form-control"
-            minLength="5"
-            required
-            ref={pwElRef}
-          />
-
-          {/* Password Revealer */}
-          <span
-            className="pw-revealer"
-            onClick={togglePwRevealer}
-            style={{ marginTop: "-37px" }}
-          >
-            {!isShowingPw && <AiFillEye />}
-            {isShowingPw && <AiFillEyeInvisible />}
+      {!token && userLoading && (
+        <div className="all-center-column">
+          <Loader type="Puff" color="#5fb0e5" height={200} width={200} />
+          <span className="mt-3 text-center">
+            Please wait while we're logging you in
           </span>
         </div>
+      )}
 
-        {/* Error message */}
-        {errMsg && <small className="text-danger d-block mt-1">{errMsg}</small>}
+      {!userLoading && (
+        <>
+          <h2 className="bold mb-4">Log In</h2>
+          <form className="form" onSubmit={handleLoginUser}>
+            <div className="form-group">
+              <label htmlFor="email" className="bold d-block">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                required
+                ref={emailElRef}
+              />
+            </div>
 
-        <div className="login-buttons mt-3">
-          <button className="btn btn-primary btn-md mr-3">Login</button>
-          <small role="button" onClick={handleGuestLogin}>
-            <u>Use guest account</u>
-          </small>
-        </div>
-      </form>
+            <div className="form-group mb-1">
+              <label htmlFor="password" className="bold d-block">
+                Password
+              </label>
+              <input
+                type={isShowingPw ? "text" : "password"}
+                className="form-control"
+                minLength="5"
+                required
+                ref={pwElRef}
+              />
+
+              {/* Password Revealer */}
+              <span
+                className="pw-revealer"
+                onClick={togglePwRevealer}
+                style={{ marginTop: "-37px" }}
+              >
+                {!isShowingPw && <AiFillEye />}
+                {isShowingPw && <AiFillEyeInvisible />}
+              </span>
+            </div>
+
+            {/* Error message */}
+            {errMsg && (
+              <small className="text-danger d-block mt-1">{errMsg}</small>
+            )}
+
+            <div className="login-buttons mt-3">
+              <button className="btn btn-primary btn-md mr-3">Login</button>
+              <small role="button" onClick={handleGuestLogin}>
+                <u>Use guest account</u>
+              </small>
+            </div>
+          </form>
+        </>
+      )}
     </>
-    // )}
-    // </>
   );
 }
