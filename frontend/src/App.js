be601dd1538fce5da6b5d3e8b0af8d9fac40e18d
Yew Kang Wei <yewyew6933@gmail.com>
2021-02-21 11:33:12 +0800

    file restructure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-// styles
-import "bootstrap/dist/css/bootstrap.css";
-import "./styles/global.scss";
-import "./styles/LandingStyles.scss";
-import "./styles/DashboardStyles.scss";
-import "react-notifications/lib/notifications.css";
-
 // pages
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { GlobalProvider } from "./context/GlobalState";
 
+// styles
+import "bootstrap/dist/css/bootstrap.css";
+import "react-notifications/lib/notifications.css";
+import "./styles/global.scss";
+
 ReactDOM.render(
   <GlobalProvider>
     <App />
